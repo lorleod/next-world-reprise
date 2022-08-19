e198851b92fa9940e0bfe4faf3a42f78e1bd6131
Taylor McLeod <Taylor@xyloMac.local>
2022-08-18 22:43:01 -0700

    completed tables schema - users, playlists, bridge. Plus seed users

--- server/db/schema/01_schema.sql
+++ server/db/schema/01_schema.sql
@@ -1,18 +1,31 @@
 -- Drop and recreate users table
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
+  id INT GENERATED ALWAYS AS IDENTITY,
   username VARCHAR(255) NOT NULL,
+  name VARCHAR(255),
   email VARCHAR(255),
   password VARCHAR(255) NOT NULL,
   is_admin BOOLEAN NOT NULL DEFAULT false
+  PRIMARY KEY (id),
 );
 
 -- Drop and recreate playlists table
 DROP TABLE IF EXISTS playlists CASCADE;
 CREATE TABLE playlists (
-  id SERIAL PRIMARY KEY NOT NULL,
+  id INT GENERATED ALWAYS AS IDENTITY,
   title VARCHAR(255) NOT NULL,
   description VARCHAR(255),
+  worlds VARCHAR(255)[],
+  PRIMARY KEY (id),
   FOREIGN KEY (user_id) INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- Drop and recreate favorites table
+DROP TABLE IF EXISTS favorites CASCADE;
+CREATE TABLE favorites (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (playlist_id) INTEGER REFERENCES playlists(id) ON DELETE CASCADE
 );
---
+++ server/db/seeds/01_seeds.sql
@@ -0,0 +1,8 @@
+-- Seed users table
+INSERT INTO users (username, email, is_admin) VALUES ('taylor', 'taylor.mcleod@gmail.com', true);
+INSERT INTO users (username, email, is_admin) VALUES ('taylor2', 'taylor.mcleod@gmail.com', true);
+INSERT INTO users (username, email, is_admin) VALUES ('Spade the Cat', 'alice@gmail.com', true);
+INSERT INTO users (username, email, is_admin) VALUES ('Lyanite', 'Lira@gmail.com', false);
+INSERT INTO users (username, email, is_admin) VALUES ('-RoFleX-', 'mike@gmail.com', false);
+INSERT INTO users (username, email, is_admin) VALUES ('Julio Summerpaw', 'julio@gmail.com', false);
+INSERT INTO users (username, email, is_admin) VALUES ('Mezillo Myker', 'mezz@gmail.com', false);
