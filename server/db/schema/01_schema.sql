95e130b1c0d64ffb46aa0ce3987f5c2294f10a49
Taylor McLeod <Taylor@xyloMac.local>
2022-08-22 16:19:13 -0700

    cleaned up sql schema and seed  statements, confirmed working

--- server/db/schema/01_schema.sql
+++ server/db/schema/01_schema.sql
@@ -5,9 +5,9 @@ CREATE TABLE users (
   username VARCHAR(255) NOT NULL,
   name VARCHAR(255),
   email VARCHAR(255),
-  password VARCHAR(255) NOT NULL,
-  is_admin BOOLEAN NOT NULL DEFAULT false
-  PRIMARY KEY (id),
+  password_hash VARCHAR(255) NOT NULL,
+  is_admin BOOLEAN NOT NULL DEFAULT false,
+  PRIMARY KEY (id)
 );
 
 -- Drop and recreate playlists table
@@ -17,15 +17,18 @@ CREATE TABLE playlists (
   title VARCHAR(255) NOT NULL,
   description VARCHAR(255),
   worlds VARCHAR(255)[],
+  creator_id INT NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (user_id) INTEGER REFERENCES users(id) ON DELETE CASCADE
+  FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- Drop and recreate favorites table
 DROP TABLE IF EXISTS favorites CASCADE;
 CREATE TABLE favorites (
   id INT GENERATED ALWAYS AS IDENTITY,
+  user_id INT NOT NULL,
+  playlist_id INT NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (user_id) INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  FOREIGN KEY (playlist_id) INTEGER REFERENCES playlists(id) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (playlist_id) REFERENCES playlists(id) ON DELETE CASCADE
 );
--- server/db/seeds/01_seeds.sql
+++ server/db/seeds/01_seeds.sql
@@ -1,8 +1,7 @@
 -- Seed users table
-INSERT INTO users (username, email, is_admin) VALUES ('taylor', 'taylor.mcleod@gmail.com', true);
-INSERT INTO users (username, email, is_admin) VALUES ('taylor2', 'taylor.mcleod@gmail.com', true);
-INSERT INTO users (username, email, is_admin) VALUES ('Spade the Cat', 'alice@gmail.com', true);
-INSERT INTO users (username, email, is_admin) VALUES ('Lyanite', 'Lira@gmail.com', false);
-INSERT INTO users (username, email, is_admin) VALUES ('-RoFleX-', 'mike@gmail.com', false);
-INSERT INTO users (username, email, is_admin) VALUES ('Julio Summerpaw', 'julio@gmail.com', false);
-INSERT INTO users (username, email, is_admin) VALUES ('Mezillo Myker', 'mezz@gmail.com', false);
+INSERT INTO users (username, password_hash, email, is_admin) VALUES ('taylor', 'qqqq1111', 'taylor.mcleod@gmail.com', true);
+INSERT INTO users (username, password_hash, email, is_admin) VALUES ('Spade the Cat', 'qqqq2222', 'alice@gmail.com', true);
+-- INSERT INTO users (username, email, is_admin) VALUES ('Lyanite', 'Lira@gmail.com', false);
+-- INSERT INTO users (username, email, is_admin) VALUES ('-RoFleX-', 'mike@gmail.com', false);
+-- INSERT INTO users (username, email, is_admin) VALUES ('Julio Summerpaw', 'julio@gmail.com', false);
+-- INSERT INTO users (username, email, is_admin) VALUES ('Mezillo Myker', 'mezz@gmail.com', false);
