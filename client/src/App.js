acd43d2f2c10a29379d732f0bb166fa02efffb84
Taylor McLeod <Taylor@xyloMac.local>
2022-08-25 17:17:20 -0700

    just playing around with some basic exercises in here

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Count: {count}
+          <div>
+            <button onClick={() => {setCount(count + 1)}}>Increment</button>
+          </div>
+
+
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
